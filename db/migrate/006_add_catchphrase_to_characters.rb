aceebe95d30e8c69b5f81d31cbef59eeeef9fb62
mio kataoka <mio.kanna@gmail.com>
2022-07-26 12:37:32 -0700

    commit tree 15773749828436691ac47cd36176c930f2cc07e6

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,7 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    change_table :characters do |t|
+    t.string :catchphrase
+    end
   end
 end
